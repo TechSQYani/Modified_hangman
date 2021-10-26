17f78f4babf20b20da964e85b17072487748c940
TechsquareJoseph <90709138+TechsquareJoseph@users.noreply.github.com>
2021-10-26 15:35:00 -0400

    Create stages.py
    A set of pixel art stages (head, torso, torso with right arm, torso with both arms, torso with both arms and one leg, & torso with both arms and both legs) for hangman, as well as a script to display each stage for two seconds on the display, could be used at game startup

---
+++ stages.py
@@ -0,0 +1,97 @@
+from sense_hat import SenseHat
+sense = SenseHat()
+
+sense.set_rotation(90)
+sense.low_light = True
+
+X = [200, 190, 0]  # Red
+O = [170, 170, 170]  # White
+
+#Define hangman "pieces"
+head = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O
+]
+
+torso = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O
+]
+
+
+arm1 = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, X, X, X, O, O, O, O,
+O, X, O, X, O, O, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O
+]
+
+arm2 = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, X, X, X, X, X, O, O,
+O, X, O, X, O, X, O, O,
+O, O, O, X, O, O, O, O,
+O, O, O, O, O, O, O, O,
+O, O, O, O, O, O, O, O
+]
+
+leg1 = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, X, X, X, X, X, O, O,
+O, X, O, X, O, X, O, O,
+O, O, O, X, O, O, O, O,
+O, O, X, O, O, O, O, O,
+O, O, X, O, O, O, O, O
+]
+
+leg2 = [
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, O, X, O, O, O, O,
+O, X, X, X, X, X, O, O,
+O, X, O, X, O, X, O, O,
+O, O, O, X, O, O, O, O,
+O, O, X, O, X, O, O, O,
+O, O, X, O, X, O, O, O
+]
+
+#Display each hangman state, with a two second delay between each.
+sense.set_pixels(head)
+time.sleep(2)
+
+sense.set_pixels(torso)
+time.sleep(2)
+
+sense.set_pixels(arm1)
+time.sleep(2)
+
+sense.set_pixels(arm2)
+time.sleep(2)
+
+sense.set_pixels(leg1)
+time.sleep(2)
+
+sense.set_pixels(leg2)
+time.sleep(2)
+
+quit()
