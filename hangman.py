b4fccc01136d2533746a6b6c2d8295bd9f154107
TechsquareJoseph <90709138+TechsquareJoseph@users.noreply.github.com>
2021-11-04 10:28:25 -0400

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,9 +1,12 @@
 #import assets for hangman
 #Original Code by Delightful Dunlin and  Eager Elk 
-# Lighting functionality written by peppe8o
+#Change lighting to use sense hat
 #Modified 10/19/2021
+#Changes since as of Nov 2: Import in letters.py as replacment for light funtion
+#Letters permits cycling through a list while displaying it o the pi
 import random
-import peppelight.py as pep
+import letters.py as alp
+import hangman_stages as display_hangman
 
 #comment out the original way to display hangman
 #def display_hangman(attempts):
@@ -73,75 +76,6 @@ import peppelight.py as pep
 #    ]
 #    return stages[attempts]
 
-#Display hangman with LEDs
-
-def display_hangman(attempts):
-    stages=[
-       [["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"],\
-       ["11111111"]
-       ["11111111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11111111"]
-       ["11111111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11000111"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11111111"]
-       ["11111111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11000001"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11111111"]
-       ["11111111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11000001"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11101111"]
-       ["11011111"]],
-
-       [["11110111"],\
-       ["11101011"],\
-       ["11110111"],\
-       ["11000001"],\
-       ["11110111"],\
-       ["11110111"],\
-       ["11101011"]
-       ["11011101"]]
-            ]
-       return stages[attempts]
-
 
 #extract random word from the text file
 def selection(name):
@@ -161,7 +95,7 @@ def word_selected_dashed():
     return ''.join(word_selected_dashed)
 
 word_selected_dashed = word_selected_dashed()
-print(word_selected_dashed)
+sense.show_message(word_selected_dashed)
 
 chances = 7
 
@@ -169,24 +103,24 @@ guessed_word = list(word_selected_dashed)
 
 while chances > 0:
     if ''.join(guessed_word) == random_word:
-        print("Congraluations, you have guessed the correct word")
+        sense.show_message("Congraluations, you have guessed the correct word", text_colour=[0,255,0])
         break
 
-    print('You have got '+ str(chances)+ ' wrong chances ')
+    sense.show_message('You have got '+ str(chances)+ ' wrong chances ', text_colour=[255,0,0])
     user_guessed_letter = input('Guess a letter >>>>> \n')
 
 
     if user_guessed_letter in random_word:
-        print('Correct!')
+        sense.show_message('Correct!', text_colour=[0,255,0])
         for i in range(len(random_word)):
             if list(random_word)[i] == user_guessed_letter:
                 guessed_word[i] = user_guessed_letter
-        print(''.join(guessed_word))
+        sense.show_message(''.join(guessed_word))
 
     elif user_guessed_letter not in random_word:
-        print('Incorrect!')
+        sense.show_message('Incorrect!', text_colour=[0,255,0])
         chances -= 1
         hang = display_hangman(attempts=(6-chances))
-        print(hang)
+        sense.show_message(hang)
 if chances == 0 :
-    print('Sorry, you have ran out of chances')
+    sense.show_message('Sorry, you have run out of chances', text_colour=[255,0,0])
